b7838ec9d6905bf660fa856096a74174b377619c
mises mind <misesmind@proton.me>
2024-07-24 06:46:47 -0700

    chore: update liscense declaration
    chore: updated liscense declaraation

--- contracts/tokens/erc5115/types/ERC5115Storage.sol
+++ contracts/tokens/erc5115/types/ERC5115Storage.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "thefactory/tokens/erc4626/types/ERC4626Storage.sol";
--- contracts/tokens/erc5115/types/ERC5115Target.sol
+++ contracts/tokens/erc5115/types/ERC5115Target.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "thefactory/tokens/erc5115/types/ERC5115Storage.sol";
